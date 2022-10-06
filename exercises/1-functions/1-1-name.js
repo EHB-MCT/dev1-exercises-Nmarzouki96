03758555f36cd3f1c563151098f7111465d232fe
Nmarzouki96 <nouredine.marzouki@student.ehb.be>
2022-10-06 16:41:44 +0200

    oefening naam

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -1,1 +1,28 @@
 "use strict";
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+drawName();
+
+function drawName() {
+
+    //N
+    context.strokeStyle = "red";
+    context.lineWidth = 5;
+    context.beginPath();
+    context.moveTo(200, 50);
+    context.lineTo(200, 250);
+    context.moveTo(200, 50);
+    context.lineTo(300, 250);
+    context.moveTo(300, 250);
+    context.lineTo(300, 50);
+    context.stroke();
+
+    ;
+
+    
+
+}
